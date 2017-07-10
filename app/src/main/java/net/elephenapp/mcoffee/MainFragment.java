0c39c446fd04a2ad5b86cd21175ac448778d345c
RobotINC <bowonmail@yahoo.comobotINC>
2017-07-10 14:24:26 +0700

    Replace Fragment

--- app/src/main/java/net/elephenapp/mcoffee/MainFragment.java
+++ app/src/main/java/net/elephenapp/mcoffee/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Bowon on 10/7/2560.
@@ -27,5 +28,28 @@ public class MainFragment extends Fragment{
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_fragment_layout, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //New Regis Controller
+        newRegisterController();
+
+
+    }
+
+    private void newRegisterController() {
+        TextView textView = (TextView) getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.maincontent, RegisterFragment.registerInstant())
+                        .commit();
+            }
+        });
     }
 }   //Main Class
