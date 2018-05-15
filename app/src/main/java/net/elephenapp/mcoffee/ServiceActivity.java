4d72e395958f57a3ec1fefdaa2dd6ce451713c42
MasterUNG <phrombutr@gmail.com>
2018-05-15 15:45:09 +0700

    Finish 15May

--- app/src/main/java/net/elephenapp/mcoffee/ServiceActivity.java
+++ app/src/main/java/net/elephenapp/mcoffee/ServiceActivity.java
@@ -1,13 +1,76 @@
 package net.elephenapp.mcoffee;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    private String jsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Get Value From Intent
+        jsonString = getIntent().getStringExtra("mid");
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemSignOut) {
+            saveSharePreferance();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    private void saveSharePreferance() {
+
+        SharedPreferences sharedPreferences = ServiceActivity.this
+                .getSharedPreferences("MyCoffee", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("mid", "");
+        editor.commit();
+
+        Intent intent = new Intent(ServiceActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service, menu);
+        return true;
+    }
+
+    private void createToolbar() {
+
+        toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("This is Title");
+        getSupportActionBar().setSubtitle(jsonString);
+
+
+
+    }
+
+}   // Main Class
